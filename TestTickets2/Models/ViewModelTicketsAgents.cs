7c13da06a6ee98b4313269dc1dad223b712fefc8
reanopp <reanopperman@gmail.com>
2017-11-21 09:41:33 +0200

    Added a get method in joined class

--- TestTickets2/Models/ViewModelTicketsAgents.cs
+++ TestTickets2/Models/ViewModelTicketsAgents.cs
@@ -5,5 +5,20 @@
         //this model is used because we are joining 2 db tables (agents & tickets). below we are defining both:
         public AgentModel AgentModel { get; set; }
         public TicketModel TicketModel { get; set; }
+
+
+        //here we do a logical check to see if AgentModel.Role matches a certain string. This is applicable per ROW returned. Used in the view.
+        //Example: in the controller we get a list of rows of all tickets and innerjoin it on agent name. Then for each of those rows, we also have an "escalated" flag.
+        public string Escalated
+        {
+            get
+            {
+                if (AgentModel.Role == "Admin")
+                {
+                    return "Yes";
+                }
+                else { return "No"; }
+            }
+        }
     }
 }
